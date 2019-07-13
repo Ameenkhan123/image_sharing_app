59d444f9b41a77c23554dfe0246180f6ab40ed65
ameenkhan123 <ameenkhana4@gmail.com>
2019-07-13 22:42:56 +0530

    Replace the place.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  # Association
-  has_many :albums, dependent: :destroy
-  has_many :images, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # Association
+  has_many :albums, dependent: :destroy
+  has_many :images, dependent: :destroy
 end
